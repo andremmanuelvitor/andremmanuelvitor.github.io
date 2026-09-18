7d8997935b1a684d77e18859242505843f77be87
Andre Emmanuel <andremmanuelvitor@gmail.com>
2026-09-18 08:02:08 -0300

    fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'andremmanuelvitor.github.io',
+  site: 'https://andremmanuelvitor.github.io',
   base: '/',
   integrations: [
     starlight({
